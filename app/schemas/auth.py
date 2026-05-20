db57e926c755130d85a6e48190ec22f8f168ef09
Yadid <youo3o0413@gmail.com>
2026-05-21 05:32:50 +0800

    add user and product schemas

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,14 +1,32 @@
-from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel, EmailStr
-from sqlalchemy.orm import Session
-from app.db.session import get_db
-from app.models.model import User
 
-class UserCreate(BaseModel):
-    username: str
+class LoginRequest(BaseModel):
     email: EmailStr
     password: str
-    
-class UserLogin(BaseModel):
-    username: str
+
+class AdminRegisterRequest(BaseModel):
+    email: EmailStr
     password: str
+    name: str
+
+class BuyerRegisterRequest(BaseModel):
+    email: EmailStr
+    password: str
+    phone: str
+    name: str
+    address: str
+
+class SellerRegisterRequest(BaseModel):
+    email: EmailStr
+    password: str
+    phone: str
+    name: str
+    company_address: str
+    company_phone: str
+    company_name: str
+
+class DriverRegisterRequest(BaseModel):
+    email: EmailStr
+    password: str
+    phone: str
+    name: str
